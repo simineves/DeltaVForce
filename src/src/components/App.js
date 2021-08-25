5ffa47181cdf964e6c452bc2e5a9e4e7f1a50bdf
anonymous33999 <meet@solvative.com>
2021-08-25 23:55:46 +0530

    Integrated phaser with react

--- src/src/components/App.js
+++ src/src/components/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom"
 
 // page imports
 import HomePage from "../pages/HomePage"
+import GamePage from '../pages/GamePage';
 
 function App() {
   return (
     <Switch>
       <Route path="/" exact component={HomePage} />
+      <Route path="/game" exact component={GamePage} />
       {/* <Route path="*" component={NotFoundPage} /> */}
     </Switch>
   )
--- src/src/pages/HomePage.js
+++ src/src/pages/HomePage.js
@@ -1,5 +1,17 @@
+import { useHistory } from 'react-router-dom'
+
 const HomePage = () => {
-  return <h2>Home Page</h2>
+
+  const history =  useHistory()
+
+  return (
+  <section style={{display: 'grid' , placeContent: 'center' , flex: 1 , margin: 10 }}>
+  <h2>Home Page</h2>
+  <button style={{margin: 10}} onClick={()=> history.push('/game')}> 
+    Play Game
+  </button>
+  </section>
+  )
 }
 
 export default HomePage
