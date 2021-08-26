d1cdd47d1207677e264c7c2cbcea0e7748d6d5a6
Nevsimao <coday.ink@gmail.com>
2021-08-26 00:55:09 -0400

    null

--- src/src/pages/GamePage.js
+++ src/src/pages/GamePage.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import Phaser from 'phaser'
 import { IonPhaser } from '@ion-phaser/react'
+import background from './assets/Background.jpeg'
 
 const Game = {
+  type: Phaser.AUTO,
   width: "100%",
   height: "100%",
-  type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+    }
+  },
   scene: {
-    init: function() {
-      this.cameras.main.setBackgroundColor('#24252A')
+    preload: function() {
+      this.load.image('background', background)
     },
     create: function() {
+      this.add.image(600, 500, 'background');
       this.helloWorld = this.add.text(
         this.cameras.main.centerX, 
         this.cameras.main.centerY, 
-        "Hello World", { 
-          font: "40px Arial", 
-          fill: "#ffffff" 
+        "DeltaVForce", { 
+          font: "30px Arial", 
+          fill: "#ffffff",
+          align: "center"
         }
       );
       this.helloWorld.setOrigin(0.5);
     },
     update: function() {
-      this.helloWorld.angle += 1;
+      this.helloWorld.rotation += 0.5;
     }
   }
 }
@@ -34,43 +44,3 @@ const GamePage = () => {
 }
 
 export default GamePage;
-
-// class GamePage extends Component {
-
-//   state = {
-//     initialize: true,
-//     game: {
-//       width: "100%",
-//       height: "100%",
-//       type: Phaser.AUTO,
-//       scene: {
-//         init: function() {
-//           this.cameras.main.setBackgroundColor('#24252A')
-//         },
-//         create: function() {
-//           this.helloWorld = this.add.text(
-//             this.cameras.main.centerX, 
-//             this.cameras.main.centerY, 
-//             "Hello World", { 
-//               font: "40px Arial", 
-//               fill: "#ffffff" 
-//             }
-//           );
-//           this.helloWorld.setOrigin(0.5);
-//         },
-//         update: function() {
-//           this.helloWorld.angle += 1;
-//         }
-//       }
-//     }
-//   }
-
-//   render() {
-//     const { initialize, game } = this.state
-//     return (
-//       <IonPhaser game={game} initialize={initialize} />
-//     )
-//   }
-// }
-
-// export default GamePage;
